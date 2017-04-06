1e9bc0f8dd88b28caf54e3de0ae50926fc144bfd
aschuldhaus <adrianschuldhaus@gmail.com>
2017-04-05 20:14:41 -0600

    Very minor change to constants.js

--- constants.js
+++ constants.js
@@ -2,7 +2,7 @@ constants = {
 
   //settings
   client_token: "YOUR_DISCORD_TOKEN", //Discord bot token
-  bot_key: 'YOUR_CHATBOT_KEY', //chat bot key
+  bot_key: "YOUR_CHATBOT_KEY", //chat bot key
   allowSpam: false, //Controls if spam filter is active
   muteTime: 900000, //Mute command duration (ms)
   command_prefix: '!', //prefix that must preceed a command
