ea6c49d7b4c46143d41d05899ae57a17073508e5
aschuldhaus <adrianschuldhaus@gmail.com>
2017-06-29 01:47:50 -0600

    Fixed: Bot's reply was 'undefined' when replying to overheard name mention.

--- src/ChatHandler.js
+++ src/ChatHandler.js
@@ -10,16 +10,14 @@ class ChatHandler {
       return;
     }
     if (message.isMentioned(this.bot.basic.user_id)){
+      console.log("hit1");
       this.bot.util.queryBotResponse(message);
       return;
     }
     const rawcontent = message.content.toLowerCase();
-    if (rawcontent.includes("\\delete")){
-      this.bot.messageCleanupQueue.add(message, 5, true);
-    }
     var username = this.bot.basic.username;
     if (rawcontent.includes(username.toLowerCase()) && !rawcontent.includes("\\" + username.toLowerCase())){
-      var response = this.bot.responses.name_mention_response;
+      var response = this.bot.responses.overheardNameResponse;
       message.reply(response)
       .then((msg) => {this.bot.messageCleanupQueue.add(msg, 0.5, true)});
       this.bot.util.logger("Responded to name mention from " + message.author.username + " on " + message.guild.name + ":" + message.channel.name);
